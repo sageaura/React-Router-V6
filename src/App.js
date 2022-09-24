ea4684fc2bc3be751a6badf88ee8d3dbb322cf02
sageaura <87586526+sageaura@users.noreply.github.com>
2022-09-24 17:01:51 +0100

    update react router dom from v5 to v6 and implement the new props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import { BrowserRouter, Link, Route, Switch } from 'react-router-dom'
-
+import { BrowserRouter, Link, Route, Routes, Navigate } from 'react-router-dom';
 // pages
 import Home from './pages/Home'
 import About from './pages/About'
 import Products from './pages/Products'
 import ProductDetails from './pages/ProductDetails'
+import {useState} from "react";
 
 function App() {
+    const [cartIsEmpty] = useState(false);
   return (
     <div className="App">
       <BrowserRouter>
@@ -16,18 +17,20 @@ function App() {
           <Link to="/about">About</Link>
           <Link to="/products">Products</Link>
         </nav>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/products/:id">
-            <ProductDetails />
-          </Route>
-          <Route path="/products">
-            <Products />
-          </Route>
-        </Switch>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about/*" element={<About />}/>
+          <Route path="/products/:id/*" element={<ProductDetails />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/test" element={(
+              <div>
+                <h2>Test</h2>
+                <p>This is a test page hi!</p>
+              </div>
+          )} />
+          <Route path="/redirect" element={<Navigate to="/about" />} />
+            <Route path="/checkout" element={cartIsEmpty ? <Navigate to="/products" /> : <p>Checkout</p> }/>
+        </Routes>
       </BrowserRouter>
     </div>
   )
